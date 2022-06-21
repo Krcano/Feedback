73b0582fc6c48ca0eed490030131da17d98dcf9c
Kelly R Cano <krcano2001@gmail.com>
2022-06-20 22:06:10 -0700

    added buttom component

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types"
 
-const Button = () => {
+const Button = ({ children, version, type, isDisabled }) => {
   return (
-    <div>Button</div>
-  )
+    <div type={type} disabled={isDisabled} className={`btn btn-${version}`} >
+      {children}
+    </div>
+  );
+};
+//  the version prop is used to deternine what css values you want for the button
+Button.defaultProps ={
+  version:"primary",
+  type:"button",
+  isDisabled:"false"
 }
 
-export default Button
+Button.propTypes={
+  children: PropTypes.node.isRequired,
+  version:PropTypes.string,
+  type: PropTypes.string,
+  isDisabled: PropTypes.bool
+}
+
+export default Button;
